5ae1eca5c00d6bfaa29d0b3e1aec32588a18caf0
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-11-04 09:34:07 -0500

    updated node server to talk with React

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-          this is header
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+state ={
+  data: null
+}
+   
+  componentDidMount(){
+    this.callsBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+
+  callsBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200){
+      throw Error(body.message)
+    }
+    return body;
+  }
+
+  render(){
+    return(
+      <section>
+        <p> 
+          {this.state.data}
+        </p>
+
+      </section>
+    )
+  }
+
 }
 
+
+
+
+
+
+// function App() {
+
+
+
+//   return (
+//     <div className="App">
+//       <header>
+//           This is React
+//       </header>
+
+
+//     </div>
+//   );
+// }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (res) => res.send('hello John'))
+// app.get('/', (res) => res.send('hello John'))
 
-app.listen(port, () => console.log('Example app listening on port 3000'))
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
 
+// create the GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'your express backend is connected'});
+})
