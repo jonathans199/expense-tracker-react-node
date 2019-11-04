f34ddf94da5851949dd9b3471f49f0ee6fcb1efb
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-11-04 16:43:58 -0500

    configured the Node server to listen to the react index.html

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+
+app.use(express.static('../client/build/')); 
 
 // app.get('/', (res) => res.send('hello John'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+app.listen(5000, () => console.log("Example app listening on port 5000"))
+
+// // old way
+// app.listen(3000, function(){
+//   console.log('listening to to port 5000')
+// })
 
 // create the GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'your express backend is connected'});
+// app.get('/express_backend', (req, res) => {
+//   res.send({ express: 'your express backend is connected'});
+// })
+
+// app.get('/express_backend', (req, res) => {
+//   res.sendFile(index.html)
+// })
+
+app.get ('/', function(req, res){
+  res.sendFile('index.html');
 })
