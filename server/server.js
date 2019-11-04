912f29a2af221f6948328dbb45bc623dbb1c8d0e
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-11-04 16:46:54 -0500

    updated to read index from react app

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('../client/build/')); 
+app.use(express.static('../client/public')); 
 
 // app.get('/', (res) => res.send('hello John'))
 
