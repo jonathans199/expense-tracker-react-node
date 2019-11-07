4b017e7377b139a2a89cad2ac1d8eb0b54fc035f
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-11-07 09:17:32 -0500

    updated input fields with State

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,51 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expenseName: "",
+      amount: "",
+      description: ""
+    };
+
+    this.handleInputs = this.handleInputs.bind(this);
+  }
+
+  handleInputs(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <section>
-        <h3> this is my react app </h3>
+        <h3> Expense project</h3>
+        <input
+          name="expenseName"
+          placeholder="Expense Name"
+          onChange={this.handleInputs}
+        />
+        <h3>{this.state.expenseName}</h3>
+        <input
+          name="amount"
+          placeholder="Amount"
+          onChange={this.handleInputs}
+        />
+        <h3>{this.state.amount}</h3>
+        <input
+          name="description"
+          placeholder="Description"
+          onChange={this.handleInputs}
+        />
+        <h3>{this.state.description}</h3> 
+        <button type="button">submit</button>
       </section>
     );
   }
 }
+
+
 
 export default App;
