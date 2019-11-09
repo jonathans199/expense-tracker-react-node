7999eb29b31263c29ad47f0ef8e1bdd75cf7da77
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-11-09 10:31:59 -0500

    updating local storage and pushing new items into array

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
 
+// localStorage.removeItem('siteData')
+
+const expenses = [];
+
 class App extends React.Component {
   constructor() {
     super();
@@ -11,6 +15,7 @@ class App extends React.Component {
     };
 
     this.handleInputs = this.handleInputs.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputs(e) {
@@ -19,34 +24,59 @@ class App extends React.Component {
     });
   }
 
+  handleSubmit() {
+    const siteData = {
+      expenseName: this.state.expenseName,
+      amount: this.state.amount,
+      description: this.state.description
+    };
+
+    localStorage.setItem("siteData", JSON.stringify(siteData));
+    
+    expenses.push(siteData);
+    console.log(expenses);
+
+    // clear State
+    this.setState({
+      expenseName: "",
+      amount: "",
+      description: ""
+    });
+
+    // clear values
+  }
+
   render() {
+    const localData = JSON.parse(localStorage.getItem("siteData"));
+
+    console.log(localData);
     return (
-      <section>
-        <h3> Expense project</h3>
-        <input
-          name="expenseName"
-          placeholder="Expense Name"
-          onChange={this.handleInputs}
-        />
-        <h3>{this.state.expenseName}</h3>
-        <input
-          name="amount"
-          placeholder="Amount"
-          onChange={this.handleInputs}
-        />
-        <h3>{this.state.amount}</h3>
-        <input
-          name="description"
-          placeholder="Description"
-          onChange={this.handleInputs}
-        />
-        <h3>{this.state.description}</h3> 
-        <button type="button">submit</button>
-      </section>
+      <div>
+        <form>
+          <h3> Expense project</h3>
+          <input
+            name="expenseName"
+            placeholder="Expense Name"
+            onChange={this.handleInputs}
+          />
+          <input
+            name="amount"
+            placeholder="Amount"
+            onChange={this.handleInputs}
+          />
+          <input
+            name="description"
+            placeholder="Description"
+            onChange={this.handleInputs}
+          />
+          <button type="button" onClick={this.handleSubmit}>
+            submit
+          </button>
+        </form>
+        <div>{expenses.map( expense => expense.expenseName)} <br /> </div>
+      </div>
     );
   }
 }
-
-
 
 export default App;
