fbdbefd94066407bb7ee2dda2e377c788a69c962
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-11-13 21:27:08 -0500

    updated expense app to pull info from localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // localStorage.removeItem('siteData')
 
-const expenses = [];
+const expenses = [{ id: 1, expenseName: "", amount: 0, description: "" }];
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      id: "",
       expenseName: "",
       amount: "",
       description: ""
@@ -16,6 +18,7 @@ class App extends React.Component {
 
     this.handleInputs = this.handleInputs.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearLocalStorage = this.clearLocalStorage.bind(this);
   }
 
   handleInputs(e) {
@@ -26,34 +29,53 @@ class App extends React.Component {
 
   handleSubmit() {
     const siteData = {
+      id: this.state.id,
       expenseName: this.state.expenseName,
       amount: this.state.amount,
       description: this.state.description
     };
 
-    localStorage.setItem("siteData", JSON.stringify(siteData));
-    
+    // push new expense into expenses array
     expenses.push(siteData);
     console.log(expenses);
 
+    // storing array as a string
+    localStorage.setItem("siteData", JSON.stringify(expenses));
+
     // clear State
     this.setState({
+      id: Date.now(),
       expenseName: "",
       amount: "",
       description: ""
     });
+  }
 
-    // clear values
+  clearLocalStorage() {
+    localStorage.removeItem("siteData");
   }
 
   render() {
+    // retriving our data and converting it back into an array
     const localData = JSON.parse(localStorage.getItem("siteData"));
 
     console.log(localData);
+
+    const allExpenses = localData.map(function(item) {
+      return (
+        <tr>
+          <th scope="row">{item.id}</th>
+          <td>{item.expenseName}</td>
+          <td>{item.description}</td>
+          <td>{item.amount}</td>
+        </tr>
+      );
+    });
+
     return (
       <div>
         <form>
-          <h3> Expense project</h3>
+          <h3>Expense project</h3>
           <input
             name="expenseName"
             placeholder="Expense Name"
@@ -73,7 +95,21 @@ class App extends React.Component {
             submit
           </button>
         </form>
-        <div>{expenses.map( expense => expense.expenseName)} <br /> </div>
+        <div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Amount</th>
+              </tr>
+            </thead>
+            <tbody>{localData ? allExpenses : "no items yet"}</tbody>
+          </table>
+          here are expenses:
+          <button onClick={this.clearLocalStorage}> Clear Local Storage</button>
+        </div>
       </div>
     );
   }
